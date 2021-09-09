d406612ecbf4b2df336ad897e9dd0e9517739953
Jeyi Adole <erthnice@gmail.com>
2021-09-09 16:54:09 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,129 @@
+
+// :root{
+   
+//     --body-font: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+//     --body-font-size:;
+//     --primary-color: #3456ff;
+//     --secondary-color:#8196ff;;
+//     --text-color:#fff;;
+//     --header-font-size: 1.5rem;
+//     --title-font-size:1rem;
+
+// }
+
+// *{
+//     box-sizing: border-box;
+//     padding: 0;
+//     margin: 0;
+
+
+// }
+
+// body{
+//     font-family: var(--body-font);
+// }
+
+// h1 {
+//     font-size: var(--header-font-size);
+// }
+
+// .container {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     height: 100vh;
+//     color: var(--text-color);
+// }
+// .card {
+//     padding: 1rem;
+//     background-color: var(--primary-color);
+//     width: 1050px;
+//     height: 600px;
+//     border-radius: 5px;
+// }
+
+// .grid {
+//     display: grid;
+//     gap: 1.5rem;
+// }
+
+// img {
+//     width: 100%;
+// }
+
+// .top {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+    
+// }
+
+// .profile-img {
+   
+//     border-radius: 50%;
+//     height: 35px;
+//     width: 35px;
+//     object-fit: cover;
+//     background-size:cover;
+//     background-position:center;
+//     margin-right: 10px;
+// }
+
+// .school-info {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+// }
+
+// .school-info .key-value {
+//     font-size: 14px;
+// }
+
+// .key-title {
+//     font-size: var(--title-font-size);
+//     display: inline-block;
+// }
+
+// .student-details .key-value {
+//     flex-direction: column;
+// }
+
+// .student-details {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+// }
+
+// .student-details .grid {
+//     align-items: center;
+//     justify-content: center;
+//     grid-template-columns:  40% 70%;
+//     margin: 5px;
+//     gap: 1px;
+    
+// }
+
+// .student-img {
+//     border-radius: 5px;
+//     height: 170px;
+//     width: 150px;
+//     object-fit: cover;
+//     background-size:cover;
+//     background-position:center;
+    
+// }
+
+// .student-info {
+//     display: flex;
+//     flex-direction: column;
+
+// }
+
+// .btn {
+//     background-color: var(--secondary-color);
+//     border: none;
+//     padding: 10px 20px;
+//     margin: 5px;
+//     border-radius: 7px;
+// }
